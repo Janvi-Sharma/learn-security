da41f3c6e573e3cfcaf2bd5f212297910f24bc56
Tund <ngductu.kma@gmail.com>
2021-10-31 22:43:31 +0700

    download  transfer programs using tcp

--- transfer.py
+++ transfer.py
@@ -72,6 +72,59 @@ class TransferFile:
                 print(split_response[-1])
             else:
                 print(response)
+    
+    def download_file(self, web_link):
+        # request
+        """GET /A1Afwc/hello.txt HTTP/2
+    Host: transfer.sh
+    User-Agent: curl/7.58.0
+    Accept: */*
+    
+    
+    """
+        # response
+        """HTTP/2 200 OK
+    Content-Disposition: attachment; filename="hello.txt"
+    Content-Type: text/plain; charset=utf-8
+    Retry-After: Thu, 28 Oct 2021 19:11:46 GMT
+    Server: Transfer.sh HTTP Server 1.0
+    X-Made-With: <3 by DutchCoders
+    X-Ratelimit-Key: 222.254.47.46
+    X-Ratelimit-Limit: 10
+    X-Ratelimit-Rate: 600
+    X-Ratelimit-Remaining: 9
+    X-Ratelimit-Reset: 1635441106
+    X-Remaining-Days: n/a
+    X-Remaining-Downloads: n/a
+    X-Served-By: Proudly served by DutchCoders
+    Content-Length: 7
+    Date: Thu, 28 Oct 2021 17:11:42 GMT
+    
+    dataaaaaaaaaa
+    """
+        # web_link = 'http://transfer.sh/WIcSx3/test.txt'
+        parse_object = urlparse(web_link)
+        address_path = parse_object.path
+        client_socket = self.client_socket
+        data_header = 'GET {} HTTP/1.1\r\nHost: transfer.sh\r\nAccept: */*\r\nUser-Agent: curl/7.68.0\r\n\r\n'.format(
+            address_path)
+        request_header = bytes(data_header, encoding='utf-8')
+        client_socket.send(request_header)
+        response = ''
+        while True:
+            recv = client_socket.recv(4096)
+            if not recv:
+                break
+            response += recv.decode("utf-8")
+            # print(response)
+            split_response = response.splitlines()
+            if split_response[0] == 'HTTP/1.1 200 OK':
+                print(split_response[-1])
+                # read file and return content-length and data
+                with open('output.txt', 'w') as f:
+                    f.write(split_response[-1])
+            else:
+                print(response)
   
 if __name__ == "__main__":
     transfer = TransferFile()
@@ -94,6 +147,11 @@ if __name__ == "__main__":
             print("\n: Enter file path")
             file_path = input()
             transfer.upload_file(file_path)
+        elif key == 3:
+            print("\n3. Download File.")
+            print("\nEnter web link")
+            web_link = input()
+            transfer.download_file(web_link)
         elif key == 0:
             print("\nYou choice exit!")
             break
